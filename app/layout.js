d74316d0c6e811624efca8cc8bf7935a96deb686
Hamza Husic <hamzahusic.dev@gmail.com>
2024-05-01 12:26:44 +0200

    Added favicon

--- app/layout.js
+++ app/layout.js
@@ -6,6 +6,9 @@ const inter = Inter({ subsets: ["latin"], weight:['400','500','600','800'] });
 export const metadata = {
   title: "Affiliate Assist",
   description: "...",
+  icons:{
+    icon:'/icon.png'
+  }
 };
 
 export default function RootLayout({ children }) {
